9fe2aaec104b101a25158d274d2389e77936293d
atelierdechile <atelierdechile@users.noreply.github.com>
2022-12-03 11:30:24 -0300

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+# VOCALIZACION
+
+
+class VocalizacionAdmin(admin.ModelAdmin):
+    list_display = ["usuario", "audio"]  # , "bpm", "beats", "timestamp"
+
+
+admin.site.register(Vocalizacion, VocalizacionAdmin)
+
+
+# MEMORICE
+
+
+class MemoriceAdmin(admin.ModelAdmin):
+    list_display = ["usuario", "acierto", "tiempo",
+                    "movimientos", "timestamp"]
+
+
+admin.site.register(Memorice, MemoriceAdmin)
+
+# EJERCICIO DE PALABRAS
+
+
+class VocalPalabrasAdmin(admin.ModelAdmin):
+    list_display = ["usuario", "audio", "timestamp"]
+
+
+admin.site.register(VocalPalabras, VocalPalabrasAdmin)
+
+# LECTURA TEXTO
+
+
+class VocalTextoAdmin(admin.ModelAdmin):
+    list_display = ["usuario", "audio", "timestamp"]
+
+
+admin.site.register(VocalTexto, VocalTextoAdmin)
